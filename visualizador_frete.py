6072f38ca948a474d8349a93034422c7f0f5c07d
Eliel Antonin <117757626+lotenapedra@users.noreply.github.com>
2023-07-27 12:47:32 -0300

    Update visualizador_frete.py

--- visualizador_frete.py
+++ visualizador_frete.py
@@ -1,13 +1,24 @@
 import streamlit as st
 import sqlite3
+from datetime import datetime
 
-st.title('Entradas')
+st.title('Gestao Entradas')
+with open("visualizacao.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+# Função para atualizar o status no banco de dados
+def atualizar_status(id, novo_status):
+    conn = sqlite3.connect('novo.db')
+    cursor = conn.cursor()
+    cursor.execute("UPDATE entrada SET Status=? WHERE ID=?", (novo_status, id))
+    conn.commit()
+    conn.close()
 
 # Conectar ao banco de dados
 conn = sqlite3.connect('novo.db')
 cursor = conn.cursor()
 
-# Executar a consulta SQL para obter os dados da tabela 'entrada'
+# Executar a consulta SQL para obter os dados da tabela 'frete'
 cursor.execute("SELECT * FROM entrada")
 data = cursor.fetchall()
 
@@ -17,65 +28,32 @@ column_names = [description[0] for description in cursor.description]
 # Fechar a conexão com o banco de dados
 conn.close()
 
-# Obter os valores únicos das colunas para os filtros
-unique_local = list(set([row[column_names.index("local")] for row in data]))
-unique_cidade_origem = list(set([row[column_names.index("cidade_origem")] for row in data]))
-unique_tipo_veiculo = list(set([row[column_names.index("tipo_veiculo")] for row in data]))
-
-# Filtros
-selected_local = st.selectbox("Filtrar por Local:", ["Todos"] + unique_local)
-selected_cidade_origem = st.selectbox("Filtrar por Cidade de Origem:", ["Todas"] + unique_cidade_origem)
-selected_tipo_veiculo = st.selectbox("Filtrar por Tipo de Veiculo:", ["Todos"] + unique_tipo_veiculo)
-
-# Filtrar os dados com base nos filtros selecionados
+# Filtro por status
+unique_statuses = list(set([row[column_names.index("Status")] for row in data]))
+selected_status = st.selectbox("Filtrar por status:", ["Todos"] + unique_statuses)
 filtered_data = data
-if selected_local != "Todos":
-    filtered_data = [row for row in filtered_data if row[column_names.index("local")] == selected_local]
-if selected_cidade_origem != "Todas":
-    filtered_data = [row for row in filtered_data if row[column_names.index("cidade_origem")] == selected_cidade_origem]
-if selected_tipo_veiculo != "Todos":
-    filtered_data = [row for row in filtered_data if row[column_names.index("tipo_veiculo")] == selected_tipo_veiculo]
+if selected_status != "Todos":
+    filtered_data = [row for row in filtered_data if row[column_names.index("Status")] == selected_status]
 
-# Criar uma tabela no Streamlit com os dados filtrados
-table = "<style>tbody tr:nth-of-type(odd) {background-color: #f5f5f5;}</style>"
-table += "<table><thead><tr>"
-for col_name in column_names:
-    table += f"<th>{col_name}</th>"
-table += "</tr></thead><tbody>"
-for row in filtered_data:
-    table += "<tr>"
-    for value in row:
-        table += f"<td>{value}</td>"
-    table += "</tr>"
-table += "</tbody></table>"
+# Filtro por motivo
+unique_motivo = list(set([row[column_names.index("motivo")] for row in data]))
+selected_motivo = st.selectbox("Filtrar por Motivo:", ["Todos"] + unique_motivo)
+if selected_motivo != "Todos":
+    filtered_data = [row for row in filtered_data if row[column_names.index("motivo")] == selected_motivo]
 
-# Exibir a tabela no Streamlit
-st.write(table, unsafe_allow_html=True)
+# Filtro por data de entrada (mostrar apenas as entradas de hoje)
+data_atual = datetime.now().strftime('%Y-%m-%d')
+filtered_data = [row for row in filtered_data if row[column_names.index("data")] == data_atual]
 
+# Atualizar o status selecionado
+col1, col2 = st.columns(2)
+with col1:
+    selected_id = st.selectbox("Para atualizar selecione o ID:", [str(row[0]) for row in filtered_data])
+with col2:
+    novo_status = st.selectbox("Selecione o novo status:", ['Liberar Entrada', 'Descarregando', 'Carregando', 'Operacao Finalizada'])
 
-
-
-
-
-
-st.title('Requisicoes de Fretes')
-#tabela de fretes requisição
-import streamlit as st
-import sqlite3
-
-# Conectar ao banco de dados
-conn = sqlite3.connect('novo.db')
-cursor = conn.cursor()
-
-# Executar a consulta SQL para obter os dados da tabela 'frete'
-cursor.execute("SELECT * FROM frete")
-data = cursor.fetchall()
-
-# Obter os nomes das colunas
-column_names = [description[0] for description in cursor.description]
-
-# Fechar a conexão com o banco de dados
-conn.close()
+if st.button('Atualizar'):
+    atualizar_status(selected_id, novo_status)
 
 # Criar uma tabela no Streamlit
 table = "<style>tbody tr:nth-of-type(odd) {background-color: #f5f5f5;}</style>"
@@ -84,29 +62,14 @@ for col_name in column_names:
     table += f"<th>{col_name}</th>"
 table += "</tr></thead><tbody>"
 
-# Função para atualizar o status no banco de dados
-def atualizar_status(id, novo_status):
-    conn = sqlite3.connect('novo.db')
-    cursor = conn.cursor()
-    cursor.execute("UPDATE frete SET Status=? WHERE ID=?", (novo_status, id))
-    conn.commit()
-    conn.close()
-
-# Obter os status únicos da coluna 'Status'
-unique_statuses = list(set([row[column_names.index("Status")] for row in data]))
-
-# Criar uma lista suspensa para selecionar o status
-selected_status = st.selectbox("Filtrar por status:", ["Todos"] + unique_statuses)
-
-# Filtrar os dados com base no status selecionado
-if selected_status != "Todos":
-    filtered_data = [row for row in data if row[column_names.index("Status")] == selected_status]
-else:
-    filtered_data = data
-
 # Exibir os dados filtrados na tabela
 for row in filtered_data:
-    table += "<tr>"
+    if row[column_names.index("Status")] == "Operacao Finalizada":
+        table += "<tr style='background-color: green; color: white;'>"
+    elif row[column_names.index("Status")] == "Liberar Entrada":
+        table += "<tr style='background-color: yellow;'>"
+    else:
+        table += "<tr>"
     for value in row:
         table += f"<td>{value}</td>"
     table += "</tr>"
@@ -114,9 +77,3 @@ table += "</tbody></table>"
 
 # Exibir a tabela no Streamlit
 st.write(table, unsafe_allow_html=True)
-
-# Atualizar o status selecionado
-selected_id = st.selectbox("Selecione o ID do frete para atualizar o status:", [str(row[0]) for row in filtered_data])
-novo_status = st.selectbox("Selecione o novo status:", ['Em Andamento', 'Realizado', 'Encerrado'])
-if st.button('Atualizar'):
-    atualizar_status(selected_id, novo_status)
