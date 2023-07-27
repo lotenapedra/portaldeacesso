7dfe57d077110bc63a9fe3b00ab7cc7bf97f5165
Eliel Antonin <117757626+lotenapedra@users.noreply.github.com>
2023-07-27 10:42:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sqlite3
 import streamlit as st
-
+st.set_page_config(page_title='Porta de Acesso',page_icon='caixa-aberta.png')
 with open("master.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
