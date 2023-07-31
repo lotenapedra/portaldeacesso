e12689da6a9d9554fd331d1e31566bbe3c2ba081
Eliel Antonin <117757626+lotenapedra@users.noreply.github.com>
2023-07-31 16:54:12 -0300

    Update home.py

--- home.py
+++ home.py
@@ -3,12 +3,14 @@ import requests
 import sqlite3
 import pandas as pd
 
+st.set_page_config(page_title='PORTAL EASY', page_icon='clean.png')
+
 
 
 with open("style.css") as f:
     st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
 
-st.set_page_config(page_title='PORTAL EASY',page_icon='clean.png')
+
 
 
 # Side bar menu
