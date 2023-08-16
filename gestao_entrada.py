df70db1677340119d334f255fd76607048a82d8c
Eliel Antonin <117757626+lotenapedra@users.noreply.github.com>
2023-08-16 18:00:15 -0300

    Update gestao_entrada.py

--- gestao_entrada.py
+++ gestao_entrada.py
@@ -2,7 +2,7 @@ import streamlit as st
 import sqlite3
 from datetime import datetime
 import csv
-import dados_csv
+import dados_csv.py
 
 st.title('Gestao Entradas')
 with open("visualizacao.css") as f:
