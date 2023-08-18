048cd768562f765ac3852929cdea777528857643
Eliel Antonin <117757626+lotenapedra@users.noreply.github.com>
2023-08-18 11:34:04 -0300

    Update gestao_entrada.py

--- gestao_entrada.py
+++ gestao_entrada.py
@@ -66,6 +66,7 @@ table += "</tbody></table>"
 #####
 import streamlit as st
 import sqlite3
+import pandas as pd
 from datetime import datetime
 
 # Resto do seu código...
@@ -73,27 +74,18 @@ from datetime import datetime
 if st.button('Atualizar'):
     atualizar_status(selected_id, novo_status)
 
-# Create an HTML table from the filtered_data
-table = "<style>tbody tr:nth-of-type(odd) {background-color: #f5f5f5;}</style>"
-table += "<table><thead><tr>"
-for col_name in column_names:
-    table += f"<th>{col_name}</th>"
-table += "</tr></thead><tbody>"
+# Create a DataFrame from the filtered_data
+df = pd.DataFrame(filtered_data, columns=column_names)
 
-for row in filtered_data:
-    if row[column_names.index("Status")] == "Operacao Finalizada":
-        table += "<tr style='background-color: green; color: white;'>"
-    elif row[column_names.index("Status")] == "Liberar Entrada":
-        table += "<tr style='background-color: yellow;'>"
-    else:
-        table += "<tr>"
-    for value in row:
-        table += f"<td>{value}</td>"
-    table += "</tr>"
-table += "</tbody></table>"
+# Display the DataFrame in Streamlit
+st.write(df)
 
-# Display the table as HTML in Streamlit
-st.write(table, unsafe_allow_html=True)
+# Allow user to generate a CSV URL
+csv_url = get_csv_url(df)
+st.write("CSV URL:", csv_url)
+def get_csv_url(df):
+    csv = df.to_csv(index=False)
+    return f"data:text/csv;charset=utf-8,{csv}"
 
 
 ####
